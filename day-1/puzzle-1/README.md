2ded20a85ed70f8134c316d3fcc2e3f1fe7c6c1b
Szymon Ciołkowski <szymon.ciolkowski@htdevelopers.com>
2018-12-02 15:49:38 +0100

    Solution for Day 1, puzzle 2

--- day-1/puzzle-1/README.md
+++ day-1/puzzle-1/README.md
@@ -1,3 +1,9 @@
+## Solve Puzzle 1
 ```elixir
 Freq.current
+```
+
+## Solve Puzzle 2
+```elixir
+Freq.seen_twice_first
 ```
---
+++ day-1/puzzle-1/puzzle-1-2.exs
@@ -0,0 +1,34 @@
+defmodule Freq do
+  @input_data_file_path "./inputdata"
+
+  # Solve Puzzle 1
+  def current, do: prepare_input |> Enum.sum
+
+  # Solve Puzzle 2
+  def seen_twice_first(start_data \\ %{0 => 1, "current_value" => 0}) do
+    prepare_input
+    |> Enum.reduce(start_data, &maybe_second_time?(&1, &2))
+    |> seen_twice_first
+  end
+
+  defp prepare_input do
+    File.stream!(@input_data_file_path)
+    |> Stream.map(&String.trim_trailing/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.to_list
+  end
+
+  defp maybe_second_time?(elem, acc) do
+    new_value = Map.get(acc, "current_value") + elem
+    acc
+    |> Map.has_key?(new_value)
+    |> found_or_continue(acc, new_value)
+  end
+
+  defp found_or_continue(true, _acc, new_value), do: IO.puts("Found! #{Integer.to_string(new_value)}")
+  defp found_or_continue(false, acc, new_value) do
+    acc
+    |> Map.put("current_value", new_value)
+    |> Map.put_new(new_value, 1)
+  end
+end
